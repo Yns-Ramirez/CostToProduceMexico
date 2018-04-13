fd1f3205e8bbe8c7386b731cf7400d5494fad46d
Yenisei Ramirez <yenisei.ramirez@xalyticsdigital.com>
2018-04-13 18:29:29 -0500

    CMEX AJuste a proces import results

--- CostToProduceMexico/Result/Import/02_create_tables_hist.sql
+++ CostToProduceMexico/Result/Import/02_create_tables_hist.sql
@@ -5,57 +5,9 @@
 -- Objective / Objetivo       : Create tables historical
 -- Subject Area / Area Sujeto : Cost to Produce
 
-CREATE TABLE IF NOT EXISTS gb_smntc_mexico_costoproducir.cp_data_detalle(
-  execution_date string, 
-  organizacion_id string, 
-  pais_id string, 
-  planta_id string, 
-  lineas_id string, 
-  turno_id string, 
-  producto_id string, 
-  rubro_id string, 
-  value double, 
-  tmoneda_id string, 
-  storeday string)
-PARTITIONED BY ( 
-  entidadlegal_id string, 
-  periodo string);
-
-CREATE TABLE IF NOT EXISTS gb_smntc_mexico_costoproducir.cp_data_piezas(
-  execution_date string, 
-  planta_id string, 
-  lineas_id string, 
-  turno_id string, 
-  producto_id string, 
-  concepto string, 
-  cantidad double, 
-  storeday string)
-PARTITIONED BY ( 
-  entidadlegal_id string, 
-  periodo string);
-
-
-CREATE TABLE IF NOT EXISTS gb_smntc_mexico_costoproducir.cp_data_sumario(
-  execution_date string, 
-  organizacion_id string, 
-  pais_id string, 
-  planta_id string, 
-  lineas_id string, 
-  turno_id string, 
-  producto_id string, 
-  rubro_id string, 
-  tipo_costo string, 
-  value double, 
-  tmoneda_id string, 
-  storeday string)
-PARTITIONED BY ( 
-  entidadlegal_id string, 
-  periodo string);
-
-
 invalidate metadata;
 
-insert overwrite table gb_smntc_mexico_costoproducir.cp_data_detalle partition(entidadlegal_id, periodo) 
+insert overwrite table jedox.cp_data_detalle partition(entidadlegal_id, periodo) 
 select 
     execution_date,
     organizacion_id,
@@ -73,7 +25,7 @@ select
 from gb_smntc_mexico_costoproducir.cp_data_detalle_last_exec;
 
 
-insert overwrite table gb_smntc_mexico_costoproducir.cp_data_piezas partition(entidadlegal_id, periodo) 
+insert overwrite table jedox.cp_data_piezas partition(entidadlegal_id, periodo) 
 select 
     execution_date,
     planta_id,
@@ -87,7 +39,7 @@ select
     regexp_replace(periodo, '\'', '')
 from gb_smntc_mexico_costoproducir.cp_data_piezas_last_exec;
 
-insert overwrite table gb_smntc_mexico_costoproducir.cp_data_sumario partition(entidadlegal_id, periodo) 
+insert overwrite table jedox.cp_data_sumario partition(entidadlegal_id, periodo) 
 select 
     execution_date, 
     organizacion_id, 
